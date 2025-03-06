5d421aed55c7538d5ea5b2b100cc777645b7d5e0
marcealita25 <conialto@gmail.com>
2025-03-06 19:14:25 +0100

    logger

--- demo/src/main/java/es/cursojava/Main.java
+++ demo/src/main/java/es/cursojava/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello Mundo world!");
         
         logger.debug("prueba logger");
     } 
