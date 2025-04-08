7d4351458e1caef9839d1b80ffaa45463016ba2d
marcealita25 <conialto@gmail.com>
2025-04-08 20:30:27 +0200

    UNO A UNO

---
+++ demo/src/main/java/es/cursojava/hibernate/dto/UnoaUno/Usuario.java
@@ -0,0 +1,54 @@
+package es.cursojava.Hibernate.dto.UnoaUno;
+
+
+/*El lado propietario de la relación es el que tiene la anotación @JoinColumn.
+mappedBy se usa en el lado no propietario, indicando el atributo en el otro lado que gestiona la relación.
+Puedes usar cascade = CascadeType.ALL para que al guardar un Usuario, se guarde también su Direccion.
+*/
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    //@OneToOne
+    @JoinColumn(name = "direccion_id") // crea la foreign key en la tabla Usuario
+    private Direccion direccion;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, Direccion direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
+
+    
+}
